bb6e7f06e03541414c637e7951897abe8061f96d
Clara Magaldi <77743669+claramagaldi@users.noreply.github.com>
2022-02-16 19:43:01 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setContact((previous) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: previous.lName,
+          email: previous.email
+        };
+      } else if (name === "lName") {
+        return {
+          fName: previous.fName,
+          lName: value,
+          email: previous.email
+        };
+      } else if (name === "email") {
+        return {
+          fName: previous.fName,
+          lName: previous.lName,
+          email: value
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
